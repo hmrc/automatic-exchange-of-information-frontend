5113781ba5a234c12f9d1687be4d5e6335af0c7f
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2023-09-28 17:35:04 +0100

    DAC6-2831: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val bootStrapVersion = "7.18.0"
+  val bootStrapVersion = "7.22.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.12.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.20.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootStrapVersion,
   )
