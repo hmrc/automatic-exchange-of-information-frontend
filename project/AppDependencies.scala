43d887601d68cd7e367d03a467630f6fded27e61
liam.henderson <35490828+liamhenderson92@users.noreply.github.com>
2024-05-21 16:55:21 +0100

    DDCYLS-5169: Remove unused dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,16 +15,11 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootStrapVersion,
-    "org.scalatest"           %% "scalatest"               % "3.2.18",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"       %% "mockito-3-4"             % "3.2.10.0",
-    "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.1",
-    "org.pegdown"             %  "pegdown"                 % "1.6.0",
     "org.jsoup"               %  "jsoup"                   % "1.17.2",
-    "org.playframework"       %% "play-test"               % PlayVersion.current,
     "org.mockito"             %% "mockito-scala"           % "1.17.31",
     "org.scalacheck"          %% "scalacheck"              % "1.18.0",
-    "com.vladsch.flexmark"    %  "flexmark-all"            % "0.64.8"
   ).map(_ % "test")
 
   def apply(): Seq[ModuleID] = compile ++ test
