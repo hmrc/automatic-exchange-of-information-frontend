5bdc352dcfadb91ddb70eef174f0035f3900812c
Nabeel Khan <176668177+NabeelK102@users.noreply.github.com>
2025-10-02 11:57:05 +0100

    DDCYLS-7216: GOV UK rebrand feature switch enabled (#34)
    Co-authored-by: NabeelK102 <NabeelK102@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,25 +1,24 @@
 import sbt.*
 
 object AppDependencies {
-  import play.core.PlayVersion
 
   val bootStrapVersion = "10.1.0"
-  val playVersion = "play-30"
+  val playVersion      = "play-30"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"       % "12.8.0",
+    "uk.gov.hmrc" %% s"play-frontend-hmrc-$playVersion"            % "12.17.0",
     "uk.gov.hmrc" %% s"play-conditional-form-mapping-$playVersion" % "3.3.0",
-    "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"        % bootStrapVersion,
+    "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion"            % bootStrapVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootStrapVersion,
-    "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
-    "org.scalatestplus"       %% "mockito-3-4"             % "3.2.10.0",
-    "org.jsoup"               %  "jsoup"                   % "1.17.2",
-    "org.mockito"             %% "mockito-scala"           % "1.17.31",
-    "org.scalacheck"          %% "scalacheck"              % "1.18.0",
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion" % bootStrapVersion,
+    "org.scalatestplus" %% "scalacheck-1-15"              % "3.2.11.0",
+    "org.scalatestplus" %% "mockito-3-4"                  % "3.2.10.0",
+    "org.jsoup"          % "jsoup"                        % "1.17.2",
+    "org.mockito"       %% "mockito-scala"                % "1.17.31",
+    "org.scalacheck"    %% "scalacheck"                   % "1.18.0"
   ).map(_ % "test")
 
   def apply(): Seq[ModuleID] = compile ++ test
